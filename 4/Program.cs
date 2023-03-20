96406f4390b51db3a8ddb2df2b029d906910a8ff
Irina <grisha9419@yandex.ru>
2023-03-20 23:30:51 +0300

    add hw4

---
+++ 4/Program.cs
@@ -0,0 +1,36 @@
+﻿//25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.Write("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень");
+// int i  = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(Math.Pow(num,i));
+
+// 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Console.Write("Введите число");
+// string  num = Console.ReadLine();
+// int L = num.Length;
+// int sum = 0;
+// for ( int i = 0; i<L; i++)
+// {
+//     sum = sum + Convert.ToInt32(num.Substring(i,1));
+// }
+// Console.Write(sum);
+
+// 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// Console.Write("Введите число ");
+
+// string  num = Console.ReadLine();
+// int l = num.Length;
+// int [] arry = new int [l];
+// for ( int i = 0; i<l; i++)
+// {
+//     arry[i] = Convert.ToInt32(num.Substring(i,1));
+    
+// }
+// Console.WriteLine(string.Join(", ", arry));
+
+
