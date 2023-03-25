34f1e01ba0ae051b52d39ee8fe4c516636345046
Irina <grisha9419@yandex.ru>
2023-03-25 23:27:20 +0300

    addhw6

---
+++ HW6/Program.cs
@@ -0,0 +1,22 @@
+﻿//43 Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Console.Write("Введитe число"); 
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введитe число"); 
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введитe число"); 
+// var b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введитe число"); 
+// var k2 = Convert.ToDouble(Console.ReadLine());
+
+// var x = -(b1 - b2)/(k1 - k2);
+// var y = (k2*b1-k1*b2)/(k2-k1);
+  
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+//41 Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = arr.Count(x => x > 0);
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
