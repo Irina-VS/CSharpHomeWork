db014d32f5ee99a427b09c7f5cd8c4b0b62db877
Irina <grisha9419@yandex.ru>
2023-04-02 23:01:28 +0300

    addparthw8

--- HW8/Program.cs
+++ HW8/Program.cs
@@ -83,4 +83,40 @@
 // Console.WriteLine($"Минимальная сумма {minsum} в строке {minnum}");
 
 
+//58. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
+// int rows =2;
+// int columns =2;
+// int [,] array1 = new int [rows,columns]; 
+// int [,] array2 = new int [rows,columns]; 
+// for (int i= 0; i< array1.GetLength(0); i++)
+// {
+//     for (int j= 0; j < array1.GetLength(1); j++)
+//     {
+//         array1[i,j]=new Random().Next(0,10);
+//         Console.Write(array1[i,j] + " ");
+//     }
+// Console.Write(" | ");
+// for (int j = 0; j < array2.GetLength(1); j++)
+//     {
+//        array2[i,j]=new Random().Next(0,10);
+//        Console.Write(array2[i,j] + " ");
+//     }
+// Console.WriteLine();
+// }
+
+// int[,] array3 = new int[array1.GetLength(0), array2.GetLength(1)];
+ 
+// for (int i = 0; i < array1.GetLength(0); ++i)
+// {
+//     for (int j = 0; j < array2.GetLength(1); ++j)
+//         {
+//             for (int k = 0; k < array2.GetLength(1); ++k)
+//                 {
+//                     array3[i, j] += array1[i, k] * array2[k, j];
+//                 }
+//         Console.Write(array3[i,j] + " ");
+//         }
+//     Console.WriteLine();
+// }
+   
